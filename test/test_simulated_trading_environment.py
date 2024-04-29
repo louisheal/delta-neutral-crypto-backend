2900ee5ab2dca4dd6fade7ea68f0433045798074
Louis Heal <louisheal@live.co.uk>
2024-04-29 18:44:22 +0100

    write test for get_price function

--- test/test_simulated_trading_environment.py
+++ test/test_simulated_trading_environment.py
@@ -1,10 +1,33 @@
+import httpretty
+import requests
 import unittest
 
+from trading_environments.simulated_trading_environment import SimulatedTradingEnvironment
 
-class SimulatedTradingEnvironmentTest(unittest.TestCase):
+
+simulated_trading_environment = SimulatedTradingEnvironment()
+
+class TestSimulatedTradingEnvironment(unittest.TestCase):
     
-    def correctly_calls_api(self):
-        pass
-    
-    def returns_correct_value(self):
-        pass
+    @httpretty.activate
+    def test_get_price_returns_correct_value(self):
+        
+        ticker = "BTC"
+        expected_price = 62944.77483424213
+        base_url = "http://example.com"
+
+        httpretty.register_uri(httpretty.GET, f"{base_url}/coins/single",
+                               body=f'{{"rate": {expected_price}}}',
+                               content_type="application/json")
+        
+        environment = SimulatedTradingEnvironment(base_url)
+
+        price = environment.get_price(ticker)
+
+        self.assertTrue(httpretty.has_request)
+        self.assertEqual(price, expected_price)
+
+        # TODO: Check details of request (request body needs to be correct)
+
+if __name__ == '__main__':
+    unittest.main()
--- trading_environments/simulated_trading_environment.py
+++ trading_environments/simulated_trading_environment.py
@@ -1,7 +1,8 @@
 from trading_environment import ITradingEnvironment
 
 
+# TODO: STE needs to take inputs (api url for example)
 class SimulatedTradingEnvironment(ITradingEnvironment):
     
-    def get_price(self, symbol):
+    def get_price(self, symbol: str) -> float:
         pass
