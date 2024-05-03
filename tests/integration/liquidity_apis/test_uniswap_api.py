37d3e15418c8d27cb2b89d405787a63f3b432152
Louis Heal <louisheal@live.co.uk>
2024-05-03 17:57:42 +0100

    write integration tests for uniswap api

---
+++ tests/integration/liquidity_apis/test_uniswap_api.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+from dotenv import load_dotenv
+
+from src.liquidity_pool_apis.uniswap_api import UniswapApi
+
+
+load_dotenv()
+
+API_KEY = os.getenv('GRAPH_API_KEY')
+APP_ID = os.getenv('UNISWAP_APP_ID')
+URL = os.getenv('GRAPH_URL')
+
+PAIR_ID = "0xa478c2975ab1ea89e8196811f51a7b7ade33eb11"
+
+
+class TestUniswapApi(unittest.TestCase):
+    
+    def setUp(self) -> None:
+        self.uniswap_api = UniswapApi(URL, APP_ID, API_KEY)
+    
+    def test_get_pair_returns_values(self):
+        
+        pair = self.uniswap_api.get_pair(PAIR_ID)
+
+        self.assertIsNotNone(pair.pair_id)
+        self.assertIsNotNone(pair.total_supply)
+        self.assertIsNotNone(pair.reserve0)
+        self.assertIsNotNone(pair.reserve1)
+        self.assertIsNotNone(pair.symbol0)
+        self.assertIsNotNone(pair.symbol1)
+        
--- tests/integration/test_simulated_trading_environment.py
+++ tests/integration/test_simulated_trading_environment.py
@@ -11,7 +11,7 @@ from src.trading_environments.simulated_trading_environment import SimulatedTrad
 
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
+API_KEY = os.getenv('COIN_API_KEY')
 BASE_URL = "https://api.livecoinwatch.com"
 TICKER = 'ETH'
 PORTFOLIO_PATH = Path('tests/integration/fixtures/test_portfolio.csv')
@@ -19,18 +19,16 @@ PORTFOLIO_PATH = Path('tests/integration/fixtures/test_portfolio.csv')
 
 class TestSimulatedTradingEnvironment(unittest.TestCase):
 
-    def setUp(self) -> None:
+    def setUp(self):
        self.coin_api = LiveCoinWatchAdapter(BASE_URL, API_KEY)
        self.portfolio = CsvPortfolio(PORTFOLIO_PATH)
        self.environment = SimulatedTradingEnvironment(self.coin_api, self.portfolio)
     
     def test_get_price_returns(self):
-        
         price = self.environment.get_price(TICKER)
         self.assertIsNotNone(price)
 
     def test_buy_coin_adds_to_portfoilio(self):
-        
         self.__setup_buy_coin_tests()
 
         result = self.environment.buy_coin(TICKER, 500)
@@ -42,7 +40,6 @@ class TestSimulatedTradingEnvironment(unittest.TestCase):
         self.assertTrue(result)
     
     def test_buy_coin_fails_when_not_enough_funds(self):
-        
         self.__setup_buy_coin_tests()
 
         result = self.environment.buy_coin(TICKER, 5000)
@@ -54,7 +51,6 @@ class TestSimulatedTradingEnvironment(unittest.TestCase):
         self.assertFalse(result)
     
     def test_sell_coin_removes_from_portfolio(self):
-        
         self.__setup_sell_coin_tests()
 
         result = self.environment.sell_coin(TICKER, 500)
@@ -66,7 +62,6 @@ class TestSimulatedTradingEnvironment(unittest.TestCase):
         self.assertTrue(result)
 
     def test_sell_coin_fails_when_not_enough_funds(self):
-        
         self.__setup_sell_coin_tests()
 
         result = self.environment.sell_coin(TICKER, 5000)
@@ -78,24 +73,21 @@ class TestSimulatedTradingEnvironment(unittest.TestCase):
         self.assertFalse(result)
     
     def __setup_buy_coin_tests(self):
-        
         portfolio = {
             'USD': 1000.0    
         }
-
-        with open(PORTFOLIO_PATH, 'w') as file:
-            csv_writer = csv.DictWriter(file, fieldnames=['Ticker','Quantity'])
-            for k, v in portfolio.items():
-                csv_writer.writerow({'Ticker':k, 'Quantity':v})
+        self.__save_portfolio(portfolio)
     
     def __setup_sell_coin_tests(self):
-        
         portfolio = {
             'USD': 0.0,
             'ETH': 500.0
         }
+        self.__save_portfolio(portfolio)
 
-        with open(PORTFOLIO_PATH, 'w') as file:
+    def __save_portfolio(self, portfolio: dict):
+        
+         with open(PORTFOLIO_PATH, 'w') as file:
             csv_writer = csv.DictWriter(file, fieldnames=['Ticker','Quantity'])
             for k, v in portfolio.items():
                 csv_writer.writerow({'Ticker':k, 'Quantity':v})
