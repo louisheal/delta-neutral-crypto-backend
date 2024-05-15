1364665e565ff48460df8a1415b046c80109419d
Louis Heal <louisheal@live.co.uk>
2024-05-15 16:55:50 +0100

    move route initialisation outside of if block

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from app.farming_pools.alpaca_finance_pool import AlpacaFinancePool
 from app.routes import Routes
 
 
-if __name__ == '__main__':
-    
-    farming_pool = AlpacaFinancePool()
-    routes = Routes(farming_pool)
+farming_pool = AlpacaFinancePool()
+routes = Routes(farming_pool)
 
-    app.run(host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+    app.run(host="localhost", debug=True)
