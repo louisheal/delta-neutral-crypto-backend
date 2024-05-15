c96bc27c063ca5f471b7ff3746f763f9db8eb1ea
Louis Heal <louisheal@live.co.uk>
2024-05-15 16:04:52 +0100

    change dependency injection pattern

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from app import app
+from app.routes import app
 from app.farming_pools.alpaca_finance_pool import AlpacaFinancePool
 
 
 FARM_POOL_API = 'FARM_POOL_API'
 
+
+app.config[FARM_POOL_API] = AlpacaFinancePool()
+
 if __name__ == '__main__':
-    alpaca_finance_api = AlpacaFinancePool()
-    app.config[FARM_POOL_API] = alpaca_finance_api
-    print(app.config[FARM_POOL_API])
     app.run(host="0.0.0.0")
