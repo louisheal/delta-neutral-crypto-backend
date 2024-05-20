9b7d03a984c034f1c63b023228cc5290a8bfa76e
Louis Heal <louisheal@live.co.uk>
2024-05-20 17:31:54 +0100

    implement flask cors and add frontend to allowed origins

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 from app import app
 from app.farming_pools.alpaca_finance_api import AlpacaFinanceApi
@@ -9,9 +10,12 @@ from app.routes import Routes
 
 load_dotenv()
 
+FRONTEND_URL = os.getenv('FRONTEND_URL')
 ALPACA_FINANCE_URL = os.getenv('ALPACA_FINANCE_URL')
 COIN_API_URL = os.getenv('COIN_API_URL')
 COIN_API_KEY = os.getenv('COIN_API_KEY')
+
+CORS(app, resources={r"/pools": {"origins": [FRONTEND_URL]}})
 
 farming_pool = AlpacaFinanceApi(ALPACA_FINANCE_URL)
 coin_api = LiveCoinWatchApi(COIN_API_URL, COIN_API_KEY)
@@ -19,4 +23,4 @@ coin_api = LiveCoinWatchApi(COIN_API_URL, COIN_API_KEY)
 routes = Routes(farming_pool, coin_api)
 
 if __name__ == '__main__':
-    app.run(host="localhost", debug=True)
+    app.run(debug=True)
