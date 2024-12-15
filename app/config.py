1df96e3a53e5f3796f990307ad5368a36eec775d
Louis Heal <louisheal@live.co.uk>
2024-12-15 21:56:04 +0000

    use kubernetes secrets for api keys

--- app/config.py
+++ app/config.py
@@ -3,12 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def get_secret(secret_name):
-    secret_path = f"/run/secrets/{secret_name}"
-    if os.path.exists(secret_path):
-        with open(secret_path, 'r') as f:
-            return f.read().strip()
-    return os.getenv(secret_name)
-
-FRONTEND_URL = get_secret('DN_FRONTEND_URL')
+FRONTEND_URL = os.getenv('DN_FRONTEND_URL')
 ALPACA_FINANCE_URL = "https://alpaca-static-api.alpacafinance.org/bsc/v1/landing/summary.json"
