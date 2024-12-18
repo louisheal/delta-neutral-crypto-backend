33c7ef63462e4bc322a606a4fd44a6bc5b8a8b43
Louis Heal <louisheal@live.co.uk>
2024-12-18 12:00:43 +0000

    update env var name

--- app/config.py
+++ app/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-FRONTEND_URL = os.getenv('DN_FRONTEND_URL')
+FRONTEND_URL = os.getenv('FRONTEND_URL')
 ALPACA_FINANCE_URL = "https://alpaca-static-api.alpacafinance.org/bsc/v1/landing/summary.json"
